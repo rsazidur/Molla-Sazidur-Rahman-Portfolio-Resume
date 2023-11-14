5b1efaa59e7a35d1b9af9cf51fbd713b92fd50bd
Molla Sazidur Rahman <rsazidur936@gmail.com>
2023-11-14 11:42:59 +0600

    Router path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Home from "./components/Home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import News from "./components/news/News";
+import Contact from "./components/contact/Contact";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
